c3f7e486c677cd8cb82d517d1cfc73398f171ebe
Moses <moseschizaram8@gmail.com>
2024-12-07 19:50:44 +0100

    Started the signup card

--- src/components/SignupCard.js
+++ src/components/SignupCard.js
@@ -1,15 +1,24 @@
 export default function SignupCard() {
   return (
     <section
-      className="relative flex flex-col items-center justify-center text-center bg-cover  bg-center"
-      style={{ backgroundImage: "url('/images/signup.jpeg')" }}
+      className="relative flex flex-col items-center justify-center text-center bg-cover bg-center p-12"
+      style={{
+        backgroundImage: "url('/images/signup.jpeg')",
+        transform: "scaleX(-1)", // Reverses only the background image
+      }}
     >
-      <h2 className="text-2xl text-b text-[#8B0000] text-center mb-8">
-        Make the Smart Move
-        <span>Today!</span>
-      </h2>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-red-600 opacity-70 z-[-1]"></div>
+      <div style={{ transform: "scaleX(-1)" }}>
+        <h2 className="text-2xl text-white font-bold text-center mb-8">
+          Make the Smart Move
+          <span> Today!</span>
+        </h2>
 
-      <p className="text-[#8B0000]">Sign Up Now </p>
+        {/* Sign Up Button */}
+        <button className="bg-white text-red-500 px-8 py-3 rounded-full font-bold">
+          Sign Up Now
+        </button>
+      </div>
     </section>
   );
 }
