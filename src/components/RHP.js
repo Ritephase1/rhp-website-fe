3ebb2fcfdb80a4ecdf9289c862e859b9c75352f3
Moses <moseschizaram8@gmail.com>
2024-12-09 15:15:31 +0100

    Add background and text color to RHP component

--- src/components/RHP.js
+++ src/components/RHP.js
@@ -1,6 +1,6 @@
 export default function RHP() {
   return (
-    <section className="text-black text-center py-12">
+    <section className="text-black bg-white text-center py-12">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 max-w-4xl mx-auto">
         {/* Image Section */}
         <div className="p-10 rounded-lg text-left flex justify-center">
@@ -12,10 +12,10 @@ export default function RHP() {
         </div>
 
         {/* Text Section */}
-        <div className="p-8 rounded-lg text-left">
+        <div className="p-8 rounded-lg bg-white text-left">
           <h3 className="text-md text-red-600 mb-4">RHP AGENT</h3>
           <h1 className="font-bold text-3xl mb-4">Who is an RHP agent</h1>
-          <small className="block mb-6 text-md leading-relaxed max-w-prose mx-auto">
+          <small className="block text-black bg-white mb-6 text-md leading-relaxed max-w-prose mx-auto">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
